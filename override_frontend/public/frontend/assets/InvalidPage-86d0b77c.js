5e05f7a3b23bb17467ebf66c74136b35c5fb88c3
Annzhii <annnzhiii@gmail.com>
2025-12-31 08:39:19 +0000

    换行调整2.0

---
+++ override_frontend/public/frontend/assets/InvalidPage-86d0b77c.js
@@ -0,0 +1,2 @@
+import{L as o}from"./LeadsIcon-b1199168.js";import{c as s,d as t,t as n,b as c,e as l,f as r}from"./index-2f1c24da.js";const i={class:"grid h-full place-items-center px-4 py-20 text-center text-lg text-ink-gray-5"},d={class:"space-y-2"},f={__name:"InvalidPage",setup(p){return(e,_)=>{const a=l("Button");return r(),s("div",i,[t("div",d,[t("div",null,n(e.__("Invalid page or not permitted to access")),1),c(a,{route:{name:"Leads"},label:e.__("Leads"),iconLeft:o},null,8,["label"])])])}}};export{f as default};
+//# sourceMappingURL=InvalidPage-86d0b77c.js.map
--- override_frontend/public/frontend/assets/global-8ce67b83.js
+++
@@ -1,2 +0,0 @@
-import{ac as c,aF as s}from"./index-a2647c31.js";const i=c("crm-global",()=>{const e=s(),{$dialog:a,$socket:l}=e.appContext.config.globalProperties;let o=()=>{};function n(t){o=t}function r(t){o(t)}return{$dialog:a,$socket:l,makeCall:r,setMakeCall:n}});export{i as g};
-//# sourceMappingURL=global-8ce67b83.js.map
